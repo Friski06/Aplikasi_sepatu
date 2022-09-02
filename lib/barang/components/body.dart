fa7ab28b8919a0462dae3d67e12344c1155ea96c
Friski06 <kiki30632@gmail.com>
2022-09-02 09:33:41 +0700

    update lagi

--- lib/barang/components/body.dart
+++ lib/barang/components/body.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:halaman_login/barang/prodak.dart';
 import 'package:halaman_login/constans.dart';
+
+import 'catagories.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
@@ -17,16 +21,69 @@ class Body extends StatelessWidget {
                 .copyWith(fontWeight: FontWeight.bold),
           ),
         ),
-        // Category(),
+        Catagories(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+            child: GridView.builder(
+              itemCount: products.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: kDefaultPaddin,
+                crossAxisSpacing: kDefaultPaddin,
+                childAspectRatio: 0.75,
+              ),
+              itemBuilder: (context, index) => itemCard(
+                product: products[index],
+                press: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Body(),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class itemCard extends StatelessWidget {
+  final Product product;
+  final Function press;
+  const itemCard({
+    Key? key,
+    required this.product,
+    required this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
         Container(
           padding: EdgeInsets.all(kDefaultPaddin),
-          height: 180,
-          width: 160,
+          // height: 180,
+          //width: 160,
           decoration: BoxDecoration(
-            color: Colors.cyan,
+            color: products[0].color,
             borderRadius: BorderRadius.circular(16),
           ),
-          child: Image.asset("assets/images/sneakers.jpg"),
+          child: Image.asset(products[0].image),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin / 4),
+          child: Text(
+            products[0].title,
+            style: TextStyle(color: kTextColor),
+          ),
+        ),
+        Text(
+          "\Rp1234",
+          style: TextStyle(fontWeight: FontWeight.bold),
         )
       ],
     );
